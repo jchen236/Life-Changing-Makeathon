c5031da0dc39ab65a6624efec250b80a77d6ea2b
Akhil Gopu <akhil.gopu72@gmail.com>
2016-02-20 22:07:38 -0500

    First version akhil

--- testing/testing.ino
+++ testing/testing.ino
@@ -1,25 +1,118 @@
+/*
+ * A0 = Motion Senser
+ * A1 = Light Senser Outside
+ * A2 = Light Senser Insde
+ */
+int stagnant = 0;
+int desiredBrightness = 70;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode (4, OUTPUT);
-
+  pinMode(1,OUTPUT); //MOTOR
+  pinMode(2, OUTPUT);   // LED
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int sensorValue = analogRead(0);
-  int aValue = analogRead(2);
-  int pValue = analogRead(3);
-  Serial.println(pValue);
-  if (pValue > 300)
-  {
-    digitalWrite(4,HIGH);
+  bool motion = false;
+  int motionValue = analogRead(A0); // Pin for motion
+  if (motionValue > 600) {
+    motion = true; 
   }
-  else
-  {
-    digitalWrite(4,LOW);
+ 
+
+  if(stagnant >= 300000) {
+    Turnoff();
+    break;
   }
-  // wait for next reading to make it more readable
-  delay(100);
+  else if(motion) {
+    stagnant = 0;
+    int lightOutside = analogRead(A1);
+    int lightInside = analogRead(A2);
+    if (desiredBrightness < lightInside + 80) {
+      StartBrighten(lightOutside,lightInside);
+    }
+    else if (desiredBrightness > lightInside - 80) {
+      StartUnbrighten(lightOutside,lightInside);
+    }
+      
+    }
+  }
+  else {
+    delay(20000);
+    stagnant+=20000;
+  }
 
 }
+
+int StartBrighten(outside, inside) {
+  
+  spinningClockwise = BlindAdjustmentClockwise(int inBrightness, int outBrightness);
+  if (desiredBrightness < lightInside + 80)
+  {
+    LightAdjustment(int inBrightness, int outBrightness);
+  }
+  
+  return spinningBrighten;
+}
+
+void LightAdjustment(int inBrightness, int outBrightness) {
+  byte brightness;
+  int sensorValue  = analogRead (A2);
+  insideLight = sensorValue
+  turnsAmount = 0; 
+  currentTime = millis();
+  while (insideLight  < desiredBrigthness) {
+    int sensorValue  = analogRead (A2);
+    insideLight = sensorValue;
+    // set the speed of pin 9:
+
+       analogWrite(4, turns);
+       analogWrite (2,turns);
+
+       // change the turnings for next time through the loop:
+
+       turns = turns + turnAmount;
+
+       // stops the speed of the motor from growing, need to determine which speed is most optimal still
+
+       if (turns == 200) { 
+           turnAmount = 0;
+       }
+  
+  
+}
+int BlindAdjustmentClockwise (int insideBrightness, int outsideBrightness)
+{
+  if (outsideBrightness < 30){
+    return 0; 
+  }
+
+  int sensorValue  = analogRead (A2);
+  insideLight = sensorValue
+  turnsAmount = 0; 
+  currentTime = millis();
+  while (insideLight  < desiredBrigthness) {
+    int sensorValue  = analogRead (A2);
+    insideLight = sensorValue;
+    // set the speed of pin 4 and 2:
+
+       analogWrite(4, turns);
+       analogWrite (2,turns);
+
+       // change the turnings for next time through the loop:
+
+       turns = turns + turnAmount;
+
+       // stops the speed of the motor from growing, need to determine which speed is most optimal still
+
+       if (turns == 200) { 
+           turnAmount = 0;
+       }
+  }
+}
+  postLoopTime = millis();
+  return postLoopTime - currentTime; //Returns a positive number to represent time spent spinning clockwise
+
+  
